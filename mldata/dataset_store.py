d5adda55fc67c9c9dc30874d356ce708ed2e0508
Adam Salvail <Adam.Salvail@USherbrooke.ca>
2014-04-09 18:07:11 -0400

    assert syntax correction

--- mldata/dataset_store.py
+++ mldata/dataset_store.py
@@ -210,8 +210,8 @@ def CSV_importer(filepath,
     meta = Metadata()
     meta.name = name
     meta.splits = splits
-    assert(len(data) == splits[-1], "The dataset read is not consistent "
-                                         "with the split given.")
+    assert len(data) == splits[-1], "The dataset read is not consistent with " \
+                                    "the split given."
     meta.nb_examples = splits[-1]
 
     dset = None
