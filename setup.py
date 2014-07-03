fc33b39cc5c13bf0b8b9a096fc6e52aa88547dcd
Marc-Alexandre Cote <marc.cote.19@gmail.com>
2014-07-03 13:58:31 -0400

    Put a basic setup.py and .travis.yml

---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(name="mldata",
+      version='0.0.0',
+      author='Adam Salvail, Marc-Alexandre Côté',
+      author_email='smart-udes-dev@googlegroups.com',
+      packages=['mldata'],
+      url='https://github.com/SMART-Lab/mldata',
+      scripts=[],
+      license='LICENSE.txt',
+
+      description="A library to manage machine learning datasets.",
+      long_description=open('README.txt').read(),
+
+      install_requires=['numpy>=1.5.0', 'h5py'],
+      tests_require=['nose'],
+      test_suite="nose.collector",
+      )
